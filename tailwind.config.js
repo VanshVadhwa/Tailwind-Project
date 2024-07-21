731bbe6b01ecd7177f80924e87766b46702da874
Vansh Vadhwa <vvad0647@gmail.com>
2024-07-21 19:37:14 +0530

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html'
+  ],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'Doraemon': "url('./img/Doraemon.jpg')"
+      // }
+    },
+  },
+  plugins: [],
+}
+
